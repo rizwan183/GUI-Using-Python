01484664879301469dbd9b36874fe72ad10ade58
rizwan183 <43751305+rizwan183@users.noreply.github.com>
2019-02-16 09:52:44 +0530

    Create Windo.py

---
+++ Windo.py
@@ -0,0 +1,5 @@
+import tkinter   #here importing tkinter package
+window=tkinter.Tk() #here creating window
+lbl=tkinter.Label(window,text="Hello! Rizwan") #here creating lbl
+lbl.pack() #here diplaying the label
+window.mainloop()
